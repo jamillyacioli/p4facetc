2b7705e96040aee8061ba1125225205dd949537d
jamillyacioli <jamillylimasantos08@gmail.com>
2025-01-13 17:11:10 -0300

    Add files via upload

---
+++ ola.rs
@@ -0,0 +1,9 @@
+fn main () {
+    let nome="patricia";
+  let nacimento=1988;
+  let ano=2024;
+  let nome1="jesus";
+  let nacimento1=1978;
+    println!("ola {},voce tem {} anos",nome,ano-nacimento); 
+    println!("e {},tem {} anos",nome1, ano-nacimento1);
+} 
